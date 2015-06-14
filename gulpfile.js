7b1a22c953bfe15aee96f414f5920ed82ce51075
Alex Langberg <alexlangberg@gmail.com>
2015-06-14 20:09:04 +0200

    eslint

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,59 @@
 
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var notify = require('gulp-notify');
+var eslint = require('gulp-eslint');
+var console = require('better-console');
+var plumber = require('gulp-plumber');
+var istanbul = require('gulp-istanbul');
+var notifier = require('node-notifier');
+var coveralls = require('gulp-coveralls');
+var error = function(task) {
+  return notify.onError({
+    message: '<%= error.message %>',
+    title: task + ' error',
+    icon: null
+  });
+};
 
-gulp.task('default', ['test'], function() {
+gulp.task('default', ['clear', 'lint', 'test']);
 
+gulp.task('watch', function() {
+  gulp.watch(['lib/**/*.js', 'test/**/*.js'], ['default']);
+});
+
+gulp.task('clear', function() {
+  console.clear();
 });
 
 gulp.task('test', function() {
   return gulp.src('test/**/*.js', {read: false})
+    .pipe(plumber({errorHandler: error('test')}))
     .pipe(mocha());
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['lib/**/*.js', 'test/**/*.js'])
+    .pipe(plumber({errorHandler: error('lint')}))
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
+
+gulp.task('coverage', function (callback) {
+  console.clear();
+  gulp.src('lib/**/*.js')
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', function () {
+      gulp.src(['test/*.js'])
+        .pipe(mocha())
+        .pipe(istanbul.writeReports())
+        .pipe(istanbul.enforceThresholds({ thresholds: { global: 100 } }))
+        .on('end', callback);
+    });
+});
+
+gulp.task('coveralls', ['lint', 'test', 'coverage'], function() {
+  return gulp.src('coverage/lcov.info').pipe(coveralls());
 });
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var should = require('chai').should();
+require('chai').should();
 var project = require('../');
 
 describe('initialization', function() {

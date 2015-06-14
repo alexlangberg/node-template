ad8fc2444f77bc3e5dffffb1d3e04152de9679ce
Alex Langberg <alexlangberg@gmail.com>
2015-06-14 17:11:41 +0200

    Mocha.

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,1 +1,11 @@
 'use strict';
+
+var should = require('chai').should();
+var project = require('../');
+
+describe('initialization', function() {
+  it('loads', function(done) {
+    project.foo.should.equal('bar');
+    done();
+  });
+});
